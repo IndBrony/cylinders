3508207b0229c0ec3a1dffbc570420e1a4d5180b
Fuad Mustamirrul Ishlah <fuad.mustamirrul@citra.digital>
2019-12-15 17:59:14 +0700

    added explanation

--- three_cylinder.go
+++ three_cylinder.go
@@ -1,11 +1,12 @@
 package cylinders
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
 
+//LeftHandCountString String wrapper for LeftHandCount function
+//input should be separated by a space. ex: "1 2 3"
 func LeftHandCountString(numsString string) (int, error) {
 	stringSlice := strings.Split(numsString, " ")
 	nums := make([]int, len(stringSlice))
@@ -20,19 +21,30 @@ func LeftHandCountString(numsString string) (int, error) {
 	return LeftHandCount(nums), nil
 }
 
+//LeftHandCount to count how many times the ball (nums) in lefthand get replaced
 func LeftHandCount(nums []int) int {
+	//variable declaration
 	aCylinder := nums
 	sCylinder := make([]int, len(nums))
 	count := 0
 
+	//for each num in nums do a loop
 	for i := 0; i < len(nums); i++ {
+		//lefthand represent the highest of the known value
+		//because the constaint of this problem is 1-1000 the 0 is its min value
 		leftHand := 0
+
+		//change the origin cylinder for a count
+		//first iteration always starts from aCylinder
 		if i%2 == 0 {
+			//foreach number in aCylinder, put the current number in the right hand
 			for _, numberInRightHand := range aCylinder {
+				//hold the highest number in the left hand
+				//and prepend the other the number to the sCylinder
+				//count everytime number on the left hand got replaced
 				if numberInRightHand > leftHand {
 					if leftHand != 0 {
 						sCylinder = append([]int{leftHand}, sCylinder[:len(nums)]...)
-						fmt.Println(numberInRightHand, leftHand)
 						count++
 					}
 					leftHand = numberInRightHand
@@ -40,14 +52,14 @@ func LeftHandCount(nums []int) int {
 					sCylinder = append([]int{numberInRightHand}, sCylinder[:len(nums)]...)
 				}
 			}
-			fmt.Println(sCylinder)
+			//emptied the used cylinder
 			aCylinder = make([]int, len(nums))
 		} else {
+			//same proccess as above but flip the cylinder
 			for _, numberInRightHand := range sCylinder {
 				if numberInRightHand > leftHand {
 					if leftHand != 0 {
 						aCylinder = append([]int{leftHand}, aCylinder[:len(nums)]...)
-						fmt.Println(numberInRightHand, leftHand)
 						count++
 					}
 					leftHand = numberInRightHand
@@ -55,7 +67,6 @@ func LeftHandCount(nums []int) int {
 					aCylinder = append([]int{numberInRightHand}, aCylinder[:len(nums)]...)
 				}
 			}
-			fmt.Println(aCylinder)
 			sCylinder = make([]int, len(nums))
 		}
 	}
